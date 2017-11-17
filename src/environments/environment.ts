f23a4a77f81ffee073e10d804a829a23c6e45acb
Vijay Deepak <vijaydeepak7195@outlook.com>
2017-11-17 20:16:40 +0530

    updated environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    
+    apiKey: "AIzaSyBsYwMrr9F9VzT8JYTXiM8iORNRe9kHlh4",
+    authDomain: "simple-a-fb-group-chat-box.firebaseapp.com",
+    databaseURL: "https://simple-a-fb-group-chat-box.firebaseio.com",
+    projectId: "simple-a-fb-group-chat-box",
+    storageBucket: "simple-a-fb-group-chat-box.appspot.com",
+    messagingSenderId: "331735728695"
   }
 };
